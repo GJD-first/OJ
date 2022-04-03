370dd7d7c9ecd1fe9a74ae0afa1befe25ddd14ab
GJD <64826216+GJD-first@users.noreply.github.com>
2022-04-03 14:31:38 +0800

    add the solution of coupon

--- AtCoder/246/Coupon.cpp
+++ AtCoder/246/Coupon.cpp
@@ -1,23 +1,42 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
-int n,k,x;
+#define ff(a) for(int i=0;i<a;i++)
+#define ll long long
+
 long long sum;
 int a[200010];
 void solve()
 {
-    int left=0;
-    sum=0;
+    int t;
+    int n,k,x;
     cin>>n>>k>>x;
-    for(int i=0;i<n;i++){cin>>a[i];sum+=a[i];}
-    for(int i=0;i<n;i++)
+    ff(n)
     {
-        if(use>=k)break;
-        use+=a[i]/x;
-        if(a[i]%x==0)left++;
+        cin>>a[i];
+        if(k==0){sum+=a[i];continue;}
+        t=a[i]/x;
+        if(t>=k)
+        {
+            a[i]=a[i]-k*x;
+            k=0;
+        }
+        else
+        {
+            a[i]=a[i]-t*x;
+            k-=t;
+        }
+        sum+=a[i];
     }
-
-
+    sort(a,a+n);
+    int i=n-1;
+    while(i>=0&&a[i]!=0&&k>0)
+    {
+        sum-=a[i];
+        i--;
+        k--;
+    }
+    cout<<sum<<endl;
 }
 int main()
 {
