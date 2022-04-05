4f5eaeb0b64a178f06329076a5e80b4d1e0bba84
GJD <64826216+GJD-first@users.noreply.github.com>
2022-04-05 10:43:18 +0800

    the sieve of Euler

--- 洛谷/模板欧拉筛.cpp
+++ 洛谷/模板欧拉筛.cpp
@@ -1,1 +1,24 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+int euler(int prime[],bool isprime[],int n)
+{
+  int len=0;
+  for(int i=2;i<=n;i++)
+  {
+    if(isprime[i])
+    {
+      prime[len++]=i;
+    }
+    for(int j=0;j<len&&i*prime[j]<=n;j++)
+    {
+      isprime[i*prime[j]]=false;
+      if(i%prime[j]==0)break;
+    }
+  }
+  return len;
+}
+int main()
+{
+  
+  return 0;
+}
